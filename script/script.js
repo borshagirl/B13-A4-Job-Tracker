60503a1d63d52b8b1657b96ad996bce0fb5c21ed
borshagirl <borshasabikunbashar@gmail.com>
2026-02-23 12:41:26 +0600

    added togglestyle and total count

--- script/script.js
+++ script/script.js
@@ -1,1 +1,46 @@
-console.log('sript file addd')
+
+let interviewList = [];
+let rejectedList = []; 
+
+
+let totalCount = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+
+const allCardsSection = document.getElementById('all-cards');
+// console.log(allCardsSection.children.length);
+const mainContainer = document.querySelector('main');
+// console.log(mainContainer);
+
+
+function calculateCount (){
+    totalCount.innerText = allCardsSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+
+}
+
+calculateCount ()
+
+
+function toggleStyle (id) {
+
+    allFilterBtn.classList.remove('bg-blue-600', 'text-white');
+    interviewFilterBtn.classList.remove('bg-blue-600', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-blue-600', 'text-white');
+
+     allFilterBtn.classList.add('bg-white', 'text-gray-500');
+    interviewFilterBtn.classList.add('bg-white', 'text-gray-500');
+    rejectedFilterBtn.classList.add('bg-white', 'text-gray-500');
+    // console.log(id);
+
+    const selected = document.getElementById(id);
+    selected.classList.add('bg-blue-600', 'text-white');
+    selected.classList.remove('bg-white', 'text-gray-500');
+
+}
